8aa36897c9335bed3b05981171c5187545a06dc0
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-12-31 15:02:03 +0100

    36. Valid Sudoku.js - Enhanced Solution

--- js/36. Valid Sudoku.js
+++ js/36. Valid Sudoku.js
@@ -3,51 +3,21 @@
  * @return {boolean}
  */
 
-
 var isValidSudoku = function (board) {
-
-    // check rows
-    for (let row = 0; row < board.length; row++) {
-        let mySet = new Set()
-        for (let col = 0; col < board[row].length; col++) {
-            let currentCell = board[row][col]
-            if (mySet.has(currentCell) && currentCell !== '.') {
-                return false
-            } else {
-                mySet.add(currentCell)
+    let seen = new Set()
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let el = board[i][j]
+            if (el !== '.') {
+                //row
+                if (seen.has('r' + i + el)) { return false } else { seen.add('r' + i + el) }
+                //col
+                if (seen.has('c' + j + el)) { return false } else { seen.add('c' + j + el) }
+                //block
+                let curBlock = '' + Math.floor(i / 3) + Math.floor(j / 3)
+                if (seen.has('c' + curBlock + el)) { return false } else { seen.add('c' + curBlock + el) }
             }
         }
     }
-
-    // check columns
-    for (let col = 0; col < board[0].length; col++) {
-        let mySet = new Set()
-        for (let row = 0; row < board.length; row++) {
-            let currentCell = board[row][col]
-            if (mySet.has(currentCell) && currentCell !== '.') {
-                return false
-            } else {
-                mySet.add(currentCell)
-            }
-
-        }
-    }
-
-    // loop big grid
-    for (let gridRow = 0; gridRow < 3; gridRow++) {
-        for (let gridCol = 0; gridCol < 3; gridCol++) {
-            let mySet = new Set()
-            for (let miniGridRow = gridRow * 3; miniGridRow < gridRow * 3 + 3; miniGridRow++) {
-                for (let miniGridCol = gridCol * 3; miniGridCol < gridCol * 3 + 3; miniGridCol++) {
-                    let currentCell = board[miniGridRow][miniGridCol]
-                    if (mySet.has(currentCell) && currentCell !== '.') {
-                        return false
-                    } else {
-                        mySet.add(currentCell)
-                    }
-                }
-            }
-        }
-    }
-    return true
+    return true;
 };

2a234f3568bfed383540fb725e7db618d3220638
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2022-01-01 16:00:12 +0100

    387. First Unique Character in a String - Enhanced Solution

--- js/387. First Unique Character in a String.js
+++ js/387. First Unique Character in a String.js
@@ -2,19 +2,14 @@
  * @param {string} s
  * @return {number}
  */
- var firstUniqChar = function (s) {
-    const string = s;
-    let mySet = new Set()
-    for (let i = 0; i < string.length; i++) {
-        if (mySet.has(string.charAt(i))) continue
-        const regexp = new RegExp(string.charAt(i), "g")
-        const matched = [...string.matchAll(regexp)]
-        if (matched.length === 1) return i
-        mySet.add(string.charAt(i))
+var firstUniqChar = function (s) {
+    for (i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i;
+        }
     }
-    return -1
+    return -1;
 };
-
 const string = "dccabaxbbcadcacdadcadcdacadtacbadcaddadd";
 console.log(firstUniqChar(string))
 
